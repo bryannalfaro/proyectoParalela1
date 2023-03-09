fe572844a51b65949ed925c194f0fd4868f0d146
Bryann Alfaro <alf19372@uvg.edu.gt>
2023-03-09 17:45:12 -0600

    Adding multiple amplitudes, periods and colors

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+//TODO
+//oscilar en ambos ejes
+//cambiar amplitudes y periodos
+//choques cambian movimiento y particiona
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
@@ -12,9 +16,9 @@ int main(int argc, char *argv[])
 {
     int windowWidth = 640;
     int windowHeight = 480;
-    int amplitud = 100;
+
     int phi = 10;
-    int N = 25;
+    int N = 50;
     bool game_is_running = true;
 
     // returns zero on success else non-zero
@@ -33,25 +37,40 @@ int main(int argc, char *argv[])
     //create an array of N  rectangles
     SDL_Rect rect[N];
     // initial position of the rectangles
-    int initial_pos[N];
-
+    int initial_pos[N][2];
+    int amplitudes[N];
+    int periods[N];
+    int colors[N][3];
+     srand(time(NULL));
     //set the properties of the rectangles
     for (int i = 0; i < N; i++) {
         rect[i].x = rand() % windowWidth;
         rect[i].y = rand() % windowWidth;
-        rect[i].w = 10;
-        rect[i].h = 10;
-        initial_pos[i] = rect[i].x;
+        int x =rand() % 50;
+        rect[i].w = x;
+        rect[i].h = x;
+        initial_pos[i][0] = rect[i].x;
+        initial_pos[i][1] = rect[i].y;
+
+        amplitudes[i] = rand() % 100;
+        periods[i] = rand() % 10;
+        //asign random colors to the rectangles;
+        colors[i][0] = rand() % 255;
+        colors[i][1] = rand() % 255;
+        colors[i][2] = rand() % 255;
+
     }
 
     double period = 2; // The period of the cosine wave
 
 
     //create color for the rectangles
-    SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+
     for (int i = 0; i < N; i++) {
+        SDL_SetRenderDrawColor(rend, colors[i][0], colors[i][1], colors[i][2], 255);
         SDL_RenderFillRect(rend, &rect[i]);
         SDL_RenderPresent(rend);
+
     }
     srand(time(NULL));
     double time_e = 0; // Time elapsed
@@ -71,8 +90,10 @@ int main(int argc, char *argv[])
 
         for (int j = 0; j < N; j++) {
             //use cosine function to move the rectangles
-            rect[j].x = initial_pos[j] + amplitud * cos(((2*M_PI)/period)*time_e+phi);
-            SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+            rect[j].x = initial_pos[j][0] + amplitudes[j] * cos(((2*M_PI)/periods[j])*time_e+phi);
+            rect[j].y = initial_pos[j][1] + amplitudes[j] * sin(((2*M_PI)/periods[j])*time_e+phi);
+            //SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+            SDL_SetRenderDrawColor(rend, colors[j][0], colors[j][1], colors[j][2], 255);
             SDL_RenderFillRect(rend, &rect[j]);
 
         }
